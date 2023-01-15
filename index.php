f4300bc182aa9d9b819bf6fed8bbc3f39aff5a40
M412k <panupat.s@ku.th>
2023-01-15 13:13:14 +0700

    php index commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        ชื่อของคุณ:<input type="text" name="name">.<br>
+        อายุของคุณ:<input type="text" name="age">.<br>
+        เพศของคุณ:
+        <input type="radio" name="gender" value="female">ผู้หญิง
+        <input type="radio" name="gender" value="male">ผู้ชาย<br>
+        สถานะของคุณ:
+        <input type="radio" name="status" value="unmarried">โสด 
+        <input type="radio" name="status" value="married">สมรส<br>
+        <input type="submit" value="Submit"><br>
+    </form>
+</body>
+</html>
